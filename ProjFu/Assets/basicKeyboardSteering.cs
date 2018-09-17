1c0b93997cc501755d99be3325c122a99be2070b
Lucas Rumney <lucasrumney94@gmail.com>
2018-09-16 19:53:12 -0700

    add basic steering

---
+++ ProjFu/Assets/basicKeyboardSteering.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicKeyboardSteering : MonoBehaviour {
+
+	public float turnSpeed = 10.0f;
+	public float turnMax = 65.0f;
+
+	public GameObject backWheel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		transform.Rotate( 0.0f, turnSpeed*Input.GetAxis("Horizontal"), 0.0f );
+		
+
+
+
+		Debug.Log(Input.GetAxis("Horizontal"));
+
+		
+	}
+}
